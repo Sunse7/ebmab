482d85ddaa32d01aa198e743a277ecf6a7350351
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2024-03-08 10:02:32 +0100

    Basic layout

--- ebmab/src/pages/project/ProjectPage.jsx
+++ ebmab/src/pages/project/ProjectPage.jsx
@@ -1,11 +1,18 @@
 import { Footer } from "../../components/footer/Footer";
 import { Header } from "../../components/header/Header";
+import { Card } from '../../components/card/Card';
+import './style.scss';
 
 export function ProjectPage() {
   return (
     <>
       <Header />
-      <h2>Project Page</h2>
+      <main className="project">
+        <h2 className="project__title">Projekt</h2>
+        <section className="project__card-container">
+          <Card />
+        </section>
+      </main>
       <Footer />
     </>
   );
