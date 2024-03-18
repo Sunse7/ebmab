116b010619d9fc1e88388fa1595cf28dd88d1d6b
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2024-03-18 17:51:08 +0100

    Login modal done, needs correct url

--- ebmab/src/pages/login/LoginPage.jsx
+++ ebmab/src/pages/login/LoginPage.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import { Header } from "../../components/header/Header";
+import { LoginModal } from "../../components/loginModal/LoginModal";
+import "./style.scss";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  async function handleLogin() {
+    const body = {
+      username,
+      password,
+    };
+    try {
+      const response = await axios.post(url, body);
+      if (response.success) {
+        navigate("/projekt/redigera");
+      } else {
+        console.error("Login failed", response);
+      }
+    } catch (err) {
+      console.error("Error", err);
+    }
+  }
+
   return (
     <>
       <Header />
-      <h2>Login Page</h2>
+      <main className="login-page">
+        <LoginModal
+          handleClick={handleLogin}
+          setPassword={setPassword}
+          setUsername={setUsername}
+        />
+      </main>
     </>
   );
 }
